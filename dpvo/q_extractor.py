a57a00de9b265541475b0880b7142ba4add7e4ae
kso <12998909+k2so3@users.noreply.github.com>
2023-04-09 19:09:36 -0400

    quantized encoder

--- dpvo/q_extractor.py
+++ dpvo/q_extractor.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
+import functools
+import copy
 class ResidualBlock(nn.Module):
     def __init__(self, in_planes, planes, norm_fn='group', stride=1):
         super(ResidualBlock, self).__init__()
@@ -114,87 +114,45 @@ class BottleneckBlock(nn.Module):
 
 DIM=32
 
-class BasicEncoder(nn.Module):
-    def __init__(self, output_dim=128, norm_fn='batch', dropout=0.0, multidim=False):
-        super(BasicEncoder, self).__init__()
-        self.norm_fn = norm_fn
-        self.multidim = multidim
+def quantize_uniform(data, n_bits, clip, device='cuda'):
+    clip = .5
+    w_c = data.clamp(-clip, clip)
+    b = torch.pow(torch.tensor(2.0), 1 - n_bits).to(device)
+    w_q = clip * torch.min(b * torch.round(w_c / (b * clip)), 1 - b)
+    return w_q
 
-        if self.norm_fn == 'group':
-            self.norm1 = nn.GroupNorm(num_groups=8, num_channels=DIM)
+def rsetattr(obj, attr, val):
+    pre, _, post = attr.rpartition('.')
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
 
-        elif self.norm_fn == 'batch':
-            self.norm1 = nn.BatchNorm2d(DIM)
 
-        elif self.norm_fn == 'instance':
-            self.norm1 = nn.InstanceNorm2d(DIM)
+def rgetattr(obj, attr, *args):
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+    return functools.reduce(_getattr, [obj] + attr.split('.'))
 
-        elif self.norm_fn == 'none':
-            self.norm1 = nn.Sequential()
 
-        self.conv1 = nn.Conv2d(3, DIM, kernel_size=7, stride=2, padding=3)
-        self.relu1 = nn.ReLU(inplace=True)
+class QConv2d(nn.Conv2d):
+    def __init__(self, quant_scheme, quant_args=None, init_args=None, b=None, *kargs, **kwargs):
+        super(QConv2d, self).__init__(*kargs, **kwargs)
+        self.weight = init_args
+        self.bias = b
+        self.quant_args = quant_args
 
-        self.in_planes = DIM
-        self.layer1 = self._make_layer(DIM,  stride=1)
-        self.layer2 = self._make_layer(2*DIM, stride=2)
-        self.layer3 = self._make_layer(4*DIM, stride=2)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
-        # output convolution
-        self.conv2 = nn.Conv2d(4*DIM, output_dim, kernel_size=1)
+    def forward(self, inputs):
+        self.quantize_params()
+        return F.conv2d(inputs, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
 
-        if self.multidim:
-            self.layer4 = self._make_layer(256, stride=2)
-            self.layer5 = self._make_layer(512, stride=2)
-
-            self.in_planes = 256
-            self.layer6 = self._make_layer(256, stride=1)
-
-            self.in_planes = 128
-            self.layer7 = self._make_layer(128, stride=1)
-
-            self.up1 = nn.Conv2d(512, 256, 1)
-            self.up2 = nn.Conv2d(256, 128, 1)
-            self.conv3 = nn.Conv2d(128, output_dim, kernel_size=1)
-
-        if dropout > 0:
-            self.dropout = nn.Dropout2d(p=dropout)
-        else:
-            self.dropout = None
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d, nn.GroupNorm)):
-                if m.weight is not None:
-                    nn.init.constant_(m.weight, 1)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, dim, stride=1):
-        layer1 = ResidualBlock(self.in_planes, dim, self.norm_fn, stride=stride)
-        layer2 = ResidualBlock(dim, dim, self.norm_fn, stride=1)
-        layers = (layer1, layer2)
-
-        self.in_planes = dim
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        b, n, c1, h1, w1 = x.shape
-        x = x.view(b*n, c1, h1, w1)
-
-        x = self.conv1(x)
-        x = self.norm1(x)
-        x = self.relu1(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-
-        x = self.conv2(x)
-
-        _, c2, h2, w2 = x.shape
-        return x.view(b, n, c2, h2, w2)
+    def quantize_params(self):
+        unquantized_weights = rgetattr(self,'weight')
+        # self.bias = rgetattr(self,'bias')
+        quantized_weights = torch.nn.Parameter(quantize_uniform(unquantized_weights,self.quant_args,1,'cpu'))
+        # print(quantized_weights)
+        rsetattr(self, 'weight', quantized_weights)
+        return
 
 
 class BasicEncoder4(nn.Module):
@@ -262,3 +220,51 @@ class BasicEncoder4(nn.Module):
 
         _, c2, h2, w2 = x.shape
         return x.view(b, n, c2, h2, w2)
+
+
+class QuantizedEncoder(nn.Module):
+    def __init__(self, quant_args):
+        super(QuantizedEncoder, self).__init__()
+        self.encoder = BasicEncoder4()
+        self.quant_args = 8
+
+    def copy_params(self, encoder):
+        self.encoder = copy.deepcopy(encoder)
+
+    def _find_conv_layers(self):
+        layers = dict(self.encoder.named_modules())
+        return layers
+
+
+    def quantize_params(self):
+
+        def quantize_helper(q_layers):
+            for item in q_layers:
+                getattr(self.encoder, item).quantize_params()
+
+        layers = self._find_conv_layers()
+        conv_layers = []
+        for item in layers:
+            if "conv" in item:
+                conv_layers.append(item)
+                W = layers[item].weight
+
+                kwargs = {
+                    "in_channels": W.shape[0],
+                    "out_channels": W.shape[1],
+                    "kernel_size": W.shape[2],
+                    "stride": layers[item].stride[0],
+                    "padding": layers[item].padding[0],
+                    "dilation": layers[item].dilation[0],
+                    "groups": layers[item].groups,
+                }
+
+                b = layers[item].bias
+                new_layer = QConv2d('uniform', 6, W, b, **kwargs)
+                setattr(self.encoder, item, new_layer)
+
+        quantize_helper(conv_layers)
+
+
+    def forward(self, x):
+        return self.encoder(x)
